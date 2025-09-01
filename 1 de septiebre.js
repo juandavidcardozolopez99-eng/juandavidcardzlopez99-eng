197edd1da0e316bf4fd265d2f7d981a50b14cb4d
juandavidcardozolopez <juandavidcardozolopez99@gmail.com>
2025-09-01 20:18:21 +0000

    clase 1 de septienbre

---
+++ 1 de septiebre.js
@@ -0,0 +1,6 @@
+function sumar(astributo1,atributo2){
+    let variableDevuelta=atributo1+atributo2
+    return variableDevuelta
+}
+let llamar_a_la_funcion=nobreDeLafuncion(1,2)
+console.log(llamar_a_la_funcion)
